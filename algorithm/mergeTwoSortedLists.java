e8e93d78fa078c598537a8f34fa8401f4d8dbdde
ctfu <cfu@my.bellevue.edu>
2016-12-04 11:20:50 -0600

    added one more solution to mergeTwoSortedLists.java

--- algorithm/mergeTwoSortedLists.java
+++ algorithm/mergeTwoSortedLists.java
@@ -21,3 +21,37 @@ public class Solution {
         }
     }
 }
+
+/* solution without recursion */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode result = new ListNode(0); /* get a reference to the head */
+        ListNode prev = result;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        if (l1 != null) {
+            prev.next = l1;
+        }
+        if (l2 != null) {
+            prev.next = l2;
+        }
+        return result.next;
+    }
+}
