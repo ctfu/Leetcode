704bb354d1bd81de8ba4f1b6c7936df2274143ed
ctfu <cfu@my.bellevue.edu>
2017-06-27 17:26:04 -0500

    more efficient implementation of maximal rectangle

--- algorithm/maximalRactangle.java
+++ algorithm/maximalRactangle.java
@@ -1,34 +1,34 @@
 /* Record the height array at each row (1 is height 1)
  * Use the technique of "Largest Rectangle in Historgram" to calculate the rectangel */
-public class Solution {
-    public int maximalRectangle(char[][] matrix) {
-        if(matrix.length == 0 || matrix[0].length == 0) return 0;
-        int rows = matrix.length, cols = matrix[0].length;
-        int[] heights = new int[cols+1];
-        heights[cols] = 0;  /* this trick is to make sure stack is empty when get out of the for loop */
-        int res = 0;
-        for(int i = 0; i < rows; i++){
-            Stack<Integer> stack = new Stack<>();
-            for(int j = 0; j < cols+1; j++){
-                if(j < cols){
-                    if(matrix[i][j] == '1'){
-                        heights[j] += 1;    /* accumulate the height */
-                    }else{
-                        heights[j] = 0;
-                    }
-                }
-                if(stack.empty() || heights[stack.peek()] < heights[j]){
-                    stack.push(j);
-                }else{
-                    while(!stack.empty() && heights[stack.peek()] >= heights[j]){
-                        int heigh = heights[stack.pop()];
-                        int width = stack.empty() ? j : (j-stack.peek()-1);
-                        res = Math.max(res, heigh * width);
-                    }
-                    stack.push(j);
-                }
-            }
-        }
-        return res;
-    }
-}
+ public class Solution {
+     public int maximalRectangle(char[][] matrix) {
+         if (matrix.length == 0 || matrix[0].length == 0) {
+             return 0;
+         }
+         int[] heights = new int[matrix[0].length + 1];
+         heights[matrix[0].length] = 0; // this is a trick to force pop the stack when reach end of the column
+         int res = 0;
+         for (int i = 0; i < matrix.length; i++) {
+             Stack<Integer> stack = new Stack<>();
+             for (int j = 0; j < matrix[i].length; j++) {
+                 if (matrix[i][j] == '1') {
+                     heights[j] += 1;
+                 } else {
+                     heights[j] = 0;    // set accumulated heights to 0
+                 }
+             }
+             // below is larget rectange in a historgram 
+             for (int k = 0; k < heights.length; k++) {
+                 if (stack.empty() || heights[stack.peek()] < heights[k]) {
+                     stack.push(k);
+                 } else {
+                     int h = heights[stack.pop()];
+                     int w = stack.empty() ? k : (k - stack.peek() - 1);
+                     res = Math.max(res, h * w);
+                     k--;
+                 }
+             }
+         }
+         return res;
+     }
+ }
