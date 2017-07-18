855917a2d287c62151efc8688515b3ee6edf2a7c
ctfu <cfu@my.bellevue.edu>
2017-07-17 23:53:08 -0500

    solved leetcode courseSchedule II

---
+++ algorithm/courseScheduleII.java
@@ -0,0 +1,49 @@
+/* Topological Sort
+*/
+public class Solution {
+    private int N = 0;
+
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] result = new int[numCourses];
+        Course[] courses = new Course[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            courses[i] = new Course(i);
+        }
+        for (int i = 0; i < prerequisites.length; i++) {
+            courses[prerequisites[i][0]].add(courses[prerequisites[i][1]]);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (isCyclic(courses[i], result)) {
+                return new int[0];
+            }
+        }
+        return result;
+    }
+
+    private boolean isCyclic(Course cur, int[] result) {
+        if (cur.tested == true) return false;
+        if (cur.visited == true) return true;
+        cur.visited = true;
+        for (Course c : cur.pre) {
+            if (isCyclic(c, result)) {
+                return true;
+            }
+        }
+        cur.tested = true;
+        result[N++] = cur.number;
+        return false;
+    }
+
+    class Course {
+        boolean visited = false;
+        boolean tested = false;
+        int number;
+        List<Course> pre = new ArrayList<Course>();
+        public Course(int i) {
+            number = i;
+        }
+        public void add(Course c) {
+            pre.add(c);
+        }
+    }
+}
