e416784a3a727748e0571ee674a816ed572cdc56
ctfu <cfu@my.bellevue.edu>
2017-07-19 23:50:05 -0500

    solved longest increasing path in matrix

---
+++ algorithm/longestIncreasingPathInMatrix.java
@@ -0,0 +1,30 @@
+/* DFS */
+public class Solution {
+    public static final int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    public int longestIncreasingPath(int[][] matrix) {
+        if(matrix.length == 0) return 0;
+        int m = matrix.length, n = matrix[0].length;
+        int[][] cache = new int[m][n];
+        int max = 1;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                int len = dfs(matrix, i, j, m, n, cache);
+                max = Math.max(max, len);
+            }
+        }
+        return max;
+    }
+
+    public int dfs(int[][] matrix, int i, int j, int m, int n, int[][] cache) {
+        if(cache[i][j] != 0) return cache[i][j];
+        int max = 1;
+        for(int[] dir: dirs) {
+            int x = i + dir[0], y = j + dir[1];
+            if(x < 0 || x >= m || y < 0 || y >= n || matrix[x][y] <= matrix[i][j]) continue;
+            int len = 1 + dfs(matrix, x, y, m, n, cache);
+            max = Math.max(max, len);
+        }
+        cache[i][j] = max;
+        return max;
+    }
+}
