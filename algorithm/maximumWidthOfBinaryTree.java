ca632413fa8a2544e07c8ef8998f7df5cfa86468
ctfu <cfu@my.bellevue.edu>
2017-08-26 00:21:31 -0500

    change solution for leetcode 662 maximum width of a binary tree

--- algorithm/maximumWidthOfBinaryTree.java
+++ algorithm/maximumWidthOfBinaryTree.java
@@ -1,18 +1,37 @@
-/* DFS */
+/* Approach: Level order traversal, utilizing the index techniques of building a heap
+* so the width is rightMostIndex - leftMostIndex + 1 */
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        return dfs(root, 0, 1, new ArrayList<Integer>(), new ArrayList<Integer>());
-    }
-
-    public int dfs(TreeNode root, int level, int order, List<Integer> start, List<Integer> end){
-        if(root == null)return 0;
-        if(start.size() == level){
-            start.add(order); end.add(order);
+        if (root == null) {
+            return 0;
         }
-        else end.set(level, order);
-        int cur = end.get(level) - start.get(level) + 1;
-        int left = dfs(root.left, level + 1, 2*order, start, end);
-        int right = dfs(root.right, level + 1, 2*order + 1, start, end);
-        return Math.max(cur, Math.max(left, right));
+        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<Integer> indexQueue = new LinkedList<>();
+        queue.offer(root);
+        indexQueue.offer(1);
+        int leftMost = 0, rightMost = 0, res = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode top = queue.poll();
+                int index = indexQueue.poll();
+                if (i == 0) {
+                    leftMost = index;
+                }
+                if (i == size - 1) {
+                    rightMost = index;
+                }
+                if (top.left != null) {
+                    queue.offer(top.left);
+                    indexQueue.offer(index * 2);
+                }
+                if (top.right != null) {
+                    queue.offer(top.right);
+                    indexQueue.offer(index * 2 + 1);
+                }
+            }
+            res = Math.max(res, rightMost - leftMost + 1);
+        }
+        return res;
     }
 }
