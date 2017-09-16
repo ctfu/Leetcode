6a920e0ba8469f0401e8d2fcb1ae8ae158cb8bff
ctfu <cfu@my.bellevue.edu>
2017-09-15 23:12:35 -0700

    leetcode 254 factor combination

---
+++ algorithm/factorCombination.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> getFactors(int n) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        helper(result, new ArrayList<Integer>(), n, 2);
+        return result;
+    }
+
+    public void helper(List<List<Integer>> result, List<Integer> item, int n, int start){
+        if (n <= 1) {
+            if (item.size() > 1) {
+                result.add(new ArrayList<Integer>(item));
+            }
+            return;
+        }
+
+        for (int i = start; i <= n; ++i) {
+            if (n % i == 0) {
+                item.add(i);
+                helper(result, item, n/i, i);
+                item.remove(item.size()-1);
+            }
+        }
+    }
+}
