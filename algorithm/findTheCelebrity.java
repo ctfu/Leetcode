c363a8b3d332750ccc5a4470d0e71855fe8f365d
ctfu <cfu@my.bellevue.edu>
2017-09-17 22:09:11 -0700

    leetcode 277 find the celebrity

---
+++ algorithm/findTheCelebrity.java
@@ -0,0 +1,16 @@
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        int candidate = 0;
+        for(int i = 1; i < n; i++){
+            if(knows(candidate, i))
+                candidate = i;
+        }
+        for(int i = 0; i < n; i++){
+            if(i != candidate && (knows(candidate, i) || !knows(i, candidate))) return -1;
+        }
+        return candidate;
+    }
+}
