c8fc1ac1701f72944740db9a0919b9390fc774b6
ctfu <cfu@my.bellevue.edu>
2017-09-21 15:28:43 -0700

    added another solution for add and search word

--- algorithm/addAndSearchWord.java
+++ algorithm/addAndSearchWord.java
@@ -1,3 +1,4 @@
+/* Approach 1: */
 public class WordDictionary {
     TrieNode root;
     /** Initialize your data structure here. */
@@ -43,6 +44,49 @@ public class WordDictionary {
         return cur.isWord;
     }
 }
+
+/* Approach 2: recursion */
+class WordDictionary {
+    private TrieNode root;
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+        TrieNode current = root;
+        for (char c : word.toCharArray()) {
+            if (current.children[c - 'a'] == null) {
+                current.children[c - 'a'] = new TrieNode();
+            }
+            current = current.children[c - 'a'];
+        }
+        current.isWord = true;
+    }
+
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public boolean search(String word) {
+        TrieNode current = root;
+        return searchHelper(word, current, 0);
+    }
+
+    public boolean searchHelper(String word, TrieNode root, int index) {
+        if (index == word.length()) {
+            return root.isWord;
+        }
+        char c = word.charAt(index);
+        if (c == '.') {
+            for (int i = 0; i < root.children.length; i++) {
+                if (root.children[i] != null && searchHelper(word, root.children[i], index + 1)) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            return root.children[c - 'a'] != null && searchHelper(word, root.children[c - 'a'], index + 1);
+        }
+    }
 
 class TrieNode {
     boolean isWord;
