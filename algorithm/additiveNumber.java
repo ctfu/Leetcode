4bfce7d8a91d541dd4e59809598ffdf9ecb4021d
ctfu <cfu@my.bellevue.edu>
2017-09-28 12:58:34 -0700

    added comment for additive number

--- algorithm/additiveNumber.java
+++ algorithm/additiveNumber.java
@@ -1,22 +1,35 @@
+/* try the possibility of first number and second number
+* the rule is that the sum result of first and second number should no less then the length of bigger number
+* so i is up to num.length() / 2 and sum.length() - i - j >= Math.max(i, j) */
 class Solution {
     public boolean isAdditiveNumber(String num) {
-        int n = num.length();
-        for (int i = 1; i <= n / 2; ++i) {
-            if (num.charAt(0) == '0' && i > 1) return false;
-            Long x1 = Long.valueOf(num.substring(0, i));
-            for (int j = 1; Math.max(j, i) <= n - i - j; ++j) {
-                if (num.charAt(i) == '0' && j > 1) break;
-                Long x2 = Long.valueOf(num.substring(i, i + j));
-                if (isValid(x1, x2, j + i, num)) return true;
+        if (num.length() < 3) {
+            return false;
+        }
+        for (int i = 1; i <= num.length() / 2; i++) {
+            if (num.charAt(0) == '0' && i > 1) {
+                return false;
+            }
+            Long first = Long.valueOf(num.substring(0, i));
+            for (int j = 1; num.length() - i - j >= Math.max(i, j); j++) {
+                if (num.charAt(i) == '0' && j > 1) {
+                    break;
+                }
+                Long second = Long.valueOf(num.substring(i, i + j));
+                if (isValid(first, second, i + j, num)) {
+                    return true;
+                }
             }
         }
         return false;
     }
-    private boolean isValid(Long x1, Long x2, int start, String num) {
-        if (start == num.length()) return true;
-        x2 = x2 + x1;
-        x1 = x2 - x1;
-        String sum = x2.toString();
-        return num.startsWith(sum, start) && isValid(x1, x2, start + sum.length(), num);
+    public boolean isValid(Long first, Long second, int start, String num) {
+        if (start == num.length()) {
+            return true;
+        }
+        second = second + first;
+        first = second - first;
+        String sum = second.toString();
+        return num.startsWith(sum, start) && isValid(first, second, start + sum.length(), num);
     }
 }
