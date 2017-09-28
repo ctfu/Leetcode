c3d0ae6a5facb1a5e24b56a93d5d693b45475742
ctfu <cfu@my.bellevue.edu>
2017-09-28 14:10:56 -0700

    added comment for binary tree maximum path sum

--- algorithm/binaryTreeMaxmimumPathSum.java
+++ algorithm/binaryTreeMaxmimumPathSum.java
@@ -1,8 +1,9 @@
 /* A path is defined as "Node sequence starting from any node to another node "
  * Each path has a highest node, it goes up from left up to highest and goes down right from hightst
- * Note: the tree value could be negative */
+ * Note: the tree value could be negative
+ * time complexity: o(n), space complexity: O(logn) */
 public class Solution {
-    int maxValue = Integer.MIN_VALUE;
+    int maxValue = Integer.MIN_VALUE; //can initialize to 0, in case there is only one node and value is negative
     public int maxPathSum(TreeNode root) {
         findMaxPath(root);
         return maxValue;
