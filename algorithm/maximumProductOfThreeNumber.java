6d6ee91d018af450dd07b239ab97613605cfe6d4
ctfu <cfu@my.bellevue.edu>
2017-10-09 10:49:25 -0700

    leetcode 628 maximum product of three number

---
+++ algorithm/maximumProductOfThreeNumber.java
@@ -0,0 +1,30 @@
+/*
+all positive - product of last three
+one negative - product of last three
+two or more negative - either product of first two and last or product of last three
+*/
+class Solution {
+    public int maximumProduct(int[] nums) {
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (num > max3) {
+                max3 = num;
+            }
+            if (num < min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2) {
+                min2 = num;
+            }
+        }
+        return Math.max(max1 * max2 * max3, max1 * min1 * min2);
+    }
+}
