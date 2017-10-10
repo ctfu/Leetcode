34c45d67c4c12a51e7ed9fc5624347a50ac7f9ac
ctfu <cfu@my.bellevue.edu>
2017-10-10 09:31:03 -0700

    added another solution for construct string from binary tree

--- algorithm/constructStringFromBinaryTree.java
+++ algorithm/constructStringFromBinaryTree.java
@@ -27,3 +27,22 @@ public class Solution {
         return sb.toString();
     }
 }
+
+/* solution 2 */
+class Solution {
+    public String tree2str(TreeNode t) {
+        if (t == null) {
+            return "";
+        }
+        if (t.left == null && t.right == null) {
+            return t.val + "";
+        }
+        if (t.left == null) {
+            return t.val + "()" + "(" + tree2str(t.right) + ")";
+        }
+        if (t.right == null) {
+            return t.val + "(" + tree2str(t.left) + ")";
+        }
+        return t.val + "(" + tree2str(t.left) + ")" + "(" + tree2str(t.right) + ")";
+    }
+}
