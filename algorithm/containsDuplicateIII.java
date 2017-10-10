0960a076dfe763b787fff879eaf032c5a579912a
ctfu <cfu@my.bellevue.edu>
2017-10-10 12:18:46 -0700

    modified solution for contains duplicate III

--- algorithm/containsDuplicateIII.java
+++ algorithm/containsDuplicateIII.java
@@ -7,19 +7,17 @@ public class Solution {
     	}
     	TreeSet<Long> set = new TreeSet<>();/* using Long to prevent integer overflow [-1, 2147483647] k = 1, t =2147483647 */
 
-    	int i = 0;
-    	while (i < nums.length) {
-    		Long floor = set.floor((long) nums[i]);
-    		Long ceiling = set.ceiling((long) nums[i]);
-    		if ((floor != null && nums[i] - floor <= t ) ||
-    				(ceiling != null && ceiling - nums[i] <= t)) {
-    			return true;
-    		}
-    		set.add((long) nums[i++]);
-    		if (i > k) {  /* k represents the indice between index i and j */
-    			set.remove((long) nums[i - k - 1]);
-    		}
-    	}
-	    return false;
+        for (int i = 0; i < nums.length; i++) {
+            Long floor = set.floor((long)nums[i]);
+            Long ceiling = set.ceiling((long)nums[i]);
+            if (floor != null && nums[i] - floor <= t || ceiling != null && ceiling - nums[i] <= t) {
+                return true;
+            }
+            set.add((long)nums[i]);
+            if (i >= k) {
+                set.remove((long)nums[i - k]); // remove the first number in the k window   
+            }
+        }
+        return false;
     }
 }
