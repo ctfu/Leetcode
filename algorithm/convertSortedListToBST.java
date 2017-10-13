a1fd169cb7d1c19cb29a02ab7ae6ad91bba0d8e6
ctfu <cfu@my.bellevue.edu>
2017-10-12 17:12:57 -0700

    added more comment for convert sorted list o binary search tree

--- algorithm/convertSortedListToBST.java
+++ algorithm/convertSortedListToBST.java
@@ -6,7 +6,10 @@ public class Solution {
     private TreeNode buildTree(ListNode head, ListNode tail){
         if(head == tail) return null;
         ListNode fast = head, slow = head;
-        /* get the middle ListNode, here null changed to tail */
+        /* get the middle ListNode, here null changed to tail
+        because we are using only one instance of the list, we
+        are only manipulating the node pointer and the tail can change to not null
+        while null is a fixed pointer */
         while(fast != tail && fast.next != tail){
             fast = fast.next.next;
             slow = slow.next;
