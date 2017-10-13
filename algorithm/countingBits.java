e0da1df9c8457b9c56c74cbbf8c243bd154dcc29
ctfu <cfu@my.bellevue.edu>
2017-10-12 23:19:15 -0700

    added comment for counting bits

--- algorithm/countingBits.java
+++ algorithm/countingBits.java
@@ -20,6 +20,7 @@ public class Solution {
     public int[] countBits(int num) {
         int[] res = new int[num + 1];
         for(int i = 1; i <= num; i++){
+            // we want to right shift one because res[i>>1] must have been calculted already
             res[i] = res[i>>1] + (i&1);
         }
         return res;
