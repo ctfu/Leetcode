78e1c28f5f2ec1661fd44a494d251753a8748366
ctfu <cfu@my.bellevue.edu>
2017-10-17 11:09:49 -0700

    added comment for judge route circle

--- algorithm/judgeRouteCircle.java
+++ algorithm/judgeRouteCircle.java
@@ -3,7 +3,7 @@ steps for "U" == steps for "D" && steps for "L" == steps for "R"
 */
 class Solution {
     public boolean judgeCircle(String moves) {
-        moves = moves + " ";
+        moves = moves + " "; // to balance split creating leading empty string
         return moves.split("U").length == moves.split("D").length &&
             moves.split("L").length == moves.split("R").length;
     }
