c111510ba3065285d2253575672d609d084e49d1
ctfu <cfu@my.bellevue.edu>
2017-10-24 23:56:08 -0700

    added comment for student attendance record

--- algorithm/studentAttendanceRecord.java
+++ algorithm/studentAttendanceRecord.java
@@ -1,5 +1,22 @@
 /* DP
-* f[i] = number of possible award strings with length i, without considering character 'A' */
+* f[i] = number of possible award strings with length i, without considering character 'A'
+
+dp[i] = total number of rewardable student records with i len
+1. without A
+1) end with P
+dp[i] = dp[i - 1]
+
+2) end with L
+...PLLL
+dp[i] = dp[i - 1] - dp[i - 4];
+
+
+2. with A
+Assume A is in index i
+[... i - 1] A [i + 1, ....n]
+dp[i - 1] * dp[n - i]
+
+*/
 class Solution {
     long M = 1000000007;
     public int checkRecord(int n) {
@@ -11,7 +28,7 @@ class Solution {
         for (int i = 4; i <= n; i++)
             f[i] = ((2 * f[i - 1]) % M + (M - f[i - 4])) % M;
         long sum = f[n]; // total number without 'A'
-        // calculate the total with A present in each index 
+        // calculate the total with A present in each index
         for (int i = 1; i <= n; i++) {
             sum += (f[i - 1] * f[n - i]) % M;
         }
