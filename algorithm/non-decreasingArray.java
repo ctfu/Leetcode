ba92c6754f078f539897349c133830749433b755
ctfu <cfu@my.bellevue.edu>
2017-10-29 21:11:09 -0700

    leetcode 665 non-decreasing array

---
+++ algorithm/non-decreasingArray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int count = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                count++;
+                if (count > 1) {
+                    return false;
+                }
+                if (i - 2 < 0 || nums[i - 2] <= nums[i]) {
+                    nums[i - 1] = nums[i];
+                } else {
+                    nums[i] = nums[i - 1];
+                }
+            }
+        }
+        return true;
+    }
+}
