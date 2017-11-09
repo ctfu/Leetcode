ad4e1b92a8f70fa4dc741b7a2b48ecdad9e26ef6
ctfu <cfu@my.bellevue.edu>
2017-11-08 21:49:31 -0800

    added time complexity for find k closest elements

--- algorithm/findKClosestElements.java
+++ algorithm/findKClosestElements.java
@@ -1,12 +1,17 @@
 /* Binary search:
-the key is to search the begin element's index of the final return k elemets */
+the key is to search the begin element's index of the final return k elemets
+time complexity: O(logn)
+space complexity: O(1)
+*/
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         List<Integer> res = new ArrayList<>();
+        // arr.length - k indicats the max index the begin elemet's index can be
         int begin = 0, end = arr.length - k;
-        while (begin < end) {
+        while (begin <= end) {
             int mid = begin + (end - begin) / 2;
             if (x > arr[mid]) {
+                //check if x is left size of right size of interval arr[mid]
                 if (x - arr[mid] > arr[mid + k] - x) {
                     begin = mid + 1;
                 } else {
