512e9fee650edb29fd2ea08f8c2ed8e95ec8c3ad
ctfu <cfu@my.bellevue.edu>
2017-11-19 19:47:02 -0800

    modifiy the solution of number of atoms

--- algorithm/numberOfAtoms.java
+++ algorithm/numberOfAtoms.java
@@ -1,40 +1,50 @@
 /* Approach: stack + treeMap */
 class Solution {
     public String countOfAtoms(String formula) {
-        int N = formula.length();
-        Stack<Map<String, Integer>> stack = new Stack();
-        stack.push(new TreeMap());
-
-        for (int i = 0; i < N;) {
-            if (formula.charAt(i) == '(') {
-                stack.push(new TreeMap());
+        Stack<TreeMap<String, Integer>> stack = new Stack<>();
+        int len = formula.length();
+        TreeMap<String, Integer> curMap = new TreeMap<>();
+        for (int i = 0; i < len;) {
+            char c = formula.charAt(i);
+            if (c == '(') {
+                stack.push(curMap);
+                curMap = new TreeMap<>();
                 i++;
-            } else if (formula.charAt(i) == ')') {
-                Map<String, Integer> top = stack.pop();
-                int iStart = ++i, multiplicity = 1;
-                while (i < N && Character.isDigit(formula.charAt(i))) i++;
-                if (i > iStart) multiplicity = Integer.parseInt(formula.substring(iStart, i));
-                for (String c: top.keySet()) {
-                    int v = top.get(c);
-                    stack.peek().put(c, stack.peek().getOrDefault(c, 0) + v * multiplicity);
+            } else if (c == ')') {
+                TreeMap<String, Integer> temp = curMap;
+                curMap = stack.pop();
+                i++;
+                int istart = i;
+                int quantity = 1;
+                while (i < len && Character.isDigit(formula.charAt(i))) i++;
+                if (i > istart) {
+                    quantity = Integer.parseInt(formula.substring(istart, i));
+                }
+                for (String name : temp.keySet()) {
+                    curMap.put(name, curMap.getOrDefault(name, 0) + temp.get(name) * quantity);
                 }
             } else {
-                int iStart = i++;
-                while (i < N && Character.isLowerCase(formula.charAt(i))) i++;
-                String name = formula.substring(iStart, i);
-                iStart = i;
-                while (i < N && Character.isDigit(formula.charAt(i))) i++;
-                int multiplicity = i > iStart ? Integer.parseInt(formula.substring(iStart, i)) : 1;
-                stack.peek().put(name, stack.peek().getOrDefault(name, 0) + multiplicity);
+                int istart = i;
+                i++;
+                while (i < len && Character.isLowerCase(formula.charAt(i))) i++;
+                String name = formula.substring(istart, i);
+                istart = i;
+                while (i < len && Character.isDigit(formula.charAt(i))) i++;
+                int quantity = 1;
+                if (i > istart) {
+                    quantity = Integer.parseInt(formula.substring(istart, i));
+                }
+                curMap.put(name, curMap.getOrDefault(name, 0) + quantity);
             }
         }
-
-        StringBuilder ans = new StringBuilder();
-        for (String name: stack.peek().keySet()) {
-            ans.append(name);
-            int multiplicity = stack.peek().get(name);
-            if (multiplicity > 1) ans.append("" + multiplicity);
+        StringBuilder sb = new StringBuilder();
+        for (String name : curMap.keySet()) {
+            int quantity = curMap.get(name);
+            sb.append(name);
+            if (quantity > 1) {
+                sb.append(quantity);
+            }
         }
-        return new String(ans);
+        return sb.toString();
     }
 }
