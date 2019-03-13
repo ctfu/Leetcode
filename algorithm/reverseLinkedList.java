85e25f7e10a7fe761cbac89afbc374aece7845cd
ctfu <cfu@my.bellevue.edu>
2019-03-13 07:13:26 -0700

    added question description

--- algorithm/reverseLinkedList.java
+++ algorithm/reverseLinkedList.java
@@ -1,3 +1,14 @@
+/*
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+
+Follow up:
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+*/
 public ListNode reverseList(ListNode head) {
     /* iterative solution */
     ListNode newHead = null;
@@ -16,9 +27,10 @@ public class Solution {
         if(head == null || head.next == null){
             return head;
         }
+        // we may have to check if newHead is null if we have not checked head.next == null previously
         ListNode newHead = reverseList(head.next);
         head.next.next = head;
         head.next = null;
         return newHead;
     }
-} 
+}
--- algorithm/reverseLinkedListII.java
+++ algorithm/reverseLinkedListII.java
@@ -1,3 +1,13 @@
+/*
+Reverse a linked list from position m to n. Do it in one-pass.
+
+Note: 1 ≤ m ≤ n ≤ length of list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL, m = 2, n = 4
+Output: 1->4->3->2->5->NULL
+*/
 /*Simply just reverse the list along the way using 4 pointers: dummy, pre, first, second*/
 /* The problem assums m, n position is valid */
 public class Solution {
