6603c44429da414e2dab3b0d8c2f45bf006db5dd
ctfu <cfu@my.bellevue.edu>
2019-03-18 23:33:14 -0700

    add another solution for symmetric tree

--- algorithm/symmetricTree.java
+++ algorithm/symmetricTree.java
@@ -1,3 +1,19 @@
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+ -------------------------------------------------------------------------------
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -21,5 +37,28 @@ public class Solution {
         }else{
             return left.val == right.val && isMirror(left.left, right.right) && isMirror(left.right, right.left);
         }
+    }
+}
+// interative Solution
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode first = queue.poll();
+            TreeNode second = queue.poll();
+            // here uses continue, their could be two null leaf nodes
+            if (first == null && second == null) continue;  
+            if (first == null || second == null) return false;
+            if (first.val != second.val) return false;
+            queue.offer(first.left);
+            queue.offer(second.right);
+            queue.offer(first.right);
+            queue.offer(second.left);
+        }
+        return true;
     }
 }
